a2e747ed3a913e83ebe64ef5fe7419ad98337ed0
Adam Holtzman <adam.holtzman@gmail.com>
2020-01-15 17:32:21 -0500

    uase react hooks to get and set data from the api, check results in console

--- src/components/random-user-list.js
+++ src/components/random-user-list.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default () => {
+  const [ people, setPeople ] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api?results=20&nat=us,ca`)
+    .then(x => x.json())
+    .then(x => setPeople(x.results))
+  }, [])
+
+  console.table(people)
+
   return(
     <div>
       <h1>random user list</h1>
