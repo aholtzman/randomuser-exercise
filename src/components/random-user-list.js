8442fc1cc69baba392b0e16095ab8a524eb7c78a
Adam Holtzman <adam.holtzman@gmail.com>
2020-01-15 18:40:16 -0500

    set basic check of date and day for birthday

--- src/components/random-user-list.js
+++ src/components/random-user-list.js
@@ -15,12 +15,25 @@ export default () => {
       <div>
         {people ? people.map((person, index) => {
           const birthday = new Date(person.dob.date)
+          const now = new Date()
+          let message = 'test test'
+
+          if (birthday.getMonth() !== now.getMonth()) {
+            console.log('month no match')
+          }
+          else if (birthday.getDate() !== now.getDate()) {
+            console.log('day no match')
+          }
+          else {
+            console.log('day and month match')
+          }
 
           return  <div key={index}>
                     <h2>{person.name.first} {person.name.last}</h2>
                     <p>Gender: {person.gender}</p>
                     <p>Country: {person.location.country}</p>
                     <p>Birthdate: {birthday.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})}</p>
+                    <p>{message}</p>
                   </div>
         }) : <h1>...loading</h1>}
       </div>
