2dfa6a67599140d2ad61c14f324276db3521c3d9
Adam Holtzman <adam.holtzman@gmail.com>
2020-01-15 18:50:05 -0500

    statndadize capitalization of the gender data point to match the rest of the print out

--- src/components/random-user-list.js
+++ src/components/random-user-list.js
@@ -18,6 +18,7 @@ export default () => {
           const now = new Date()
           let message = 'test test'
           const namePosessive = person.name.first[person.name.first.length - 1] === 's' ? `${person.name.first}'` : `${person.name.first}'s`
+          const genId = person.gender[0].toUpperCase() + person.gender.slice(1).toLowerCase()
 
           if (birthday.getMonth() !== now.getMonth()) {
             message = (birthday.getMonth() > now.getMonth()) ? 'has yet to occur' : 'already happened'
@@ -31,7 +32,7 @@ export default () => {
 
           return  <div key={index}>
                     <h2>{person.name.first} {person.name.last}</h2>
-                    <p>Gender: {person.gender}</p>
+                    <p>Gender: {genId}</p>
                     <p>Country: {person.location.country}</p>
                     <p>Birthdate: {birthday.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})}</p>
                     <p>{namePosessive} birthday {message} {message === 'is today(!)' ? '' : 'this year.'}</p>
