83085d375d26acaf2625c1decf608fc2c649dba3
Adam Holtzman <adam.holtzman@gmail.com>
2020-01-15 17:49:56 -0500

    format bithdate to be more readable

--- src/components/random-user-list.js
+++ src/components/random-user-list.js
@@ -14,12 +14,13 @@ export default () => {
       <h1>random user list</h1>
       <div>
         {people ? people.map((person, index) => {
-          console.log(person)
+          const birthday = new Date(person.dob.date)
+
           return  <div key={index}>
                     <h2>{person.name.first} {person.name.last}</h2>
                     <p>Gender: {person.gender}</p>
                     <p>Country: {person.location.country}</p>
-                    <p>Birthdate: {person.dob.date}</p>
+                    <p>Birthdate: {birthday.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric'})}</p>
                   </div>
         }) : <h1>...loading</h1>}
       </div>
